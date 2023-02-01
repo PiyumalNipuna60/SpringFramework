e0113e5a0e745ef2bf5e492e0096b6254ee32d74
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-01 09:31:59 +0530

    Set Pojo Class

---
+++ SpringFramework/01-Hello-Spring/src/lk/ijse/Spring/pojo/PojoOne.java
@@ -0,0 +1,12 @@
+package lk.ijse.Spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PojoOne {
+
+    public PojoOne(){
+        System.out.println("PojoOne: Instantiated..!");
+
+    }
+}
