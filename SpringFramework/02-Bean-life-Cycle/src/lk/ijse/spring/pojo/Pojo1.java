9589d20453083b24ec61981357d6de53ed90b4b9
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-01 19:40:40 +0530

    Add Bean Factory Aware Invoked

--- SpringFramework/02-Bean-life-Cycle/src/lk/ijse/spring/pojo/Pojo1.java
+++ SpringFramework/02-Bean-life-Cycle/src/lk/ijse/spring/pojo/Pojo1.java
@@ -1,16 +1,24 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Pojo1 implements BeanNameAware {
+public class Pojo1 implements BeanNameAware, BeanFactoryAware {
     public Pojo1(){
         System.out.println("Pojo1 Constructor");
     }
 
     @Override
     public void setBeanName(String name) {
-        System.out.println("Bean Name Avear! : "+name);
+        System.out.println("Bean Name Aware! : "+name);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factor Aware : "+beanFactory);
     }
 }
