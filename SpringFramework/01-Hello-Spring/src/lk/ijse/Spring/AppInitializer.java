8c1938c4f07ed3cd934d4679f7307608b4140b17
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-01 10:36:57 +0530

    Add Context Invocation

--- SpringFramework/01-Hello-Spring/src/lk/ijse/Spring/AppInitializer.java
+++ SpringFramework/01-Hello-Spring/src/lk/ijse/Spring/AppInitializer.java
@@ -9,9 +9,15 @@ public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ant = new AnnotationConfigApplicationContext();
         ant.register(AppConfig.class);
-
-
         ant.refresh();
+
+        //Context Invocation
+        Customer bean = ant.getBean(Customer.class);
+        System.out.println(bean);
+
+        bean.getCustomerName();
+
+
         ant.close();
     }
 }
