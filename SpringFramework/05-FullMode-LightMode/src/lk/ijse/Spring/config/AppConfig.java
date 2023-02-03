d4075fc8c3a73f7404070c56029ac81bbd58d8fe
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-04 01:29:51 +0530

    Model 05-FullMode-LightMode : Add Light Mode

--- SpringFramework/05-FullMode-LightMode/src/lk/ijse/Spring/config/AppConfig.java
+++ SpringFramework/05-FullMode-LightMode/src/lk/ijse/Spring/config/AppConfig.java
@@ -13,19 +13,19 @@ public class AppConfig {
         System.out.println("AppConfig Constructor..!");
     }
 
-    @Bean
-    //Full Mode
-    public Pojo2 pojo2(){
-        //Inter-bean Dependency Invocation
-        Pojo3 pojo31 = pojo3();
-        Pojo3 pojo32 = pojo3();
-        System.out.println(pojo31);
-        System.out.println(pojo32);
-        return new Pojo2();
-    }
-
-    @Bean
-    public Pojo3 pojo3(){
-        return new Pojo3();
-    }
+//    @Bean
+//    //Full Mode
+//    public Pojo2 pojo2(){
+//        //Inter-bean Dependency Invocation
+//        Pojo3 pojo31 = pojo3();
+//        Pojo3 pojo32 = pojo3();
+//        System.out.println(pojo31);
+//        System.out.println(pojo32);
+//        return new Pojo2();
+//    }
+//
+//    @Bean
+//    public Pojo3 pojo3(){
+//        return new Pojo3();
+//    }
 }
--- SpringFramework/05-FullMode-LightMode/src/lk/ijse/Spring/pojo/Pojo1.java
+++ SpringFramework/05-FullMode-LightMode/src/lk/ijse/Spring/pojo/Pojo1.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -37,5 +38,21 @@ public class Pojo1 implements BeanNameAware, BeanFactoryAware, ApplicationContex
     @Override
     public void destroy() throws Exception {
         System.out.println("Pojo1 : DisposableBean");
+    }
+
+    @Bean
+    //Light Mode
+    public Pojo2 pojo2(){
+        //Inter-bean Dependency Invocation
+        Pojo3 pojo31 = pojo3();
+        Pojo3 pojo32 = pojo3();
+        System.out.println(pojo31);
+        System.out.println(pojo32);
+        return new Pojo2();
+    }
+
+    @Bean
+    public Pojo3 pojo3(){
+        return new Pojo3();
     }
 }
