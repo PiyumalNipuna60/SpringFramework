9d19f2586d934b622fba42081a7e94e60dc3fbc7
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-04 01:16:46 +0530

    Model 05-FullMode-LightMode : Add

--- SpringFramework/05-FullMode-LightMode/src/lk/ijse/Spring/config/AppConfig.java
+++ SpringFramework/05-FullMode-LightMode/src/lk/ijse/Spring/config/AppConfig.java
@@ -1,5 +1,8 @@
 package lk.ijse.Spring.config;
 
+import lk.ijse.Spring.pojo.Pojo2;
+import lk.ijse.Spring.pojo.Pojo3;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +11,17 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     public AppConfig(){
         System.out.println("AppConfig Constructor..!");
+    }
+
+    @Bean
+
+    public Pojo2 pojo2(){
+
+        return new Pojo2();
+    }
+
+    @Bean
+    public Pojo3 pojo3(){
+        return new Pojo3();
     }
 }
--- SpringFramework/05-FullMode-LightMode/src/lk/ijse/Spring/pojo/Pojo3.java
+++ SpringFramework/05-FullMode-LightMode/src/lk/ijse/Spring/pojo/Pojo3.java
@@ -1,41 +1,42 @@
 package lk.ijse.Spring.pojo;
 
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
 
-@Component
-public class Pojo1 implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+public class Pojo3 implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
-    public Pojo1(){
-        System.out.println("Pojo1 Constructor..!");
+    public Pojo3(){
+        System.out.println("Pojo3 Constructor..!");
     }
 
     @Override
     public void setBeanName(String name) {
-        System.out.println("Pojo1 Bean Name Aware! : "+name);
+        System.out.println("Pojo3 Bean Name Aware! : "+name);
     }
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("Pojo1 Bean Factor Aware : "+beanFactory);
+        System.out.println("Pojo3 Bean Factor Aware : "+beanFactory);
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("Pojo1 applicationContext Aware : "+applicationContext);
+        System.out.println("Pojo3 applicationContext Aware : "+applicationContext);
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println("Pojo1  : InitializingBean bean");
+        System.out.println("Pojo3  : InitializingBean bean");
         System.out.println("==========================");
+
+
     }
 
     @Override
     public void destroy() throws Exception {
-        System.out.println("Pojo1 : DisposableBean");
+        System.out.println("Pojo3 : DisposableBean");
     }
 }
