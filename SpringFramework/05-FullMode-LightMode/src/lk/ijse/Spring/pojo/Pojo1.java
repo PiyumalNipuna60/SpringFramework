45fd9741b0efeb77a15e5c33c55cc2279bb9c834
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-03 22:57:16 +0530

    Model 05-FullMode-LightMode : Add Pojo class

---
+++ SpringFramework/05-FullMode-LightMode/src/lk/ijse/Spring/pojo/Pojo1.java
@@ -0,0 +1,11 @@
+package lk.ijse.Spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Pojo1 {
+
+    public Pojo1(){
+        System.out.println("Pojo1 Constructor..!");
+    }
+}
