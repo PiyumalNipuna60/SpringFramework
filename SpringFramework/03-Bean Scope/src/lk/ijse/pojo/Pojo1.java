65755af14ae8de707d98f2edac952f4571091f5d
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-03 18:51:11 +0530

    Add Pojo : set Life sycle

--- SpringFramework/03-Bean Scope/src/lk/ijse/pojo/Pojo1.java
+++ SpringFramework/03-Bean Scope/src/lk/ijse/pojo/Pojo1.java
@@ -14,26 +14,30 @@ public class Pojo1 implements BeanNameAware, BeanFactoryAware, ApplicationContex
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-
+        System.out.println("4 Bean setBeanFactory");
     }
 
     @Override
     public void setBeanName(String name) {
+        System.out.println("3 Bean setBeanName");
 
     }
 
     @Override
     public void destroy() throws Exception {
+        System.out.println("7 DisposableBean");
 
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        System.out.println("6 InitializingBean");
 
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("5 ApplicationContextAware");
 
     }
 }
