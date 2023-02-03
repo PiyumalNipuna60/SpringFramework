0ad4ccb4a2013cc0c5f9dc72cc49b26eee8217f5
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-03 18:46:16 +0530

    Add Pojo class

--- SpringFramework/03-Bean Scope/src/lk/ijse/AppInitializer.java
+++ SpringFramework/03-Bean Scope/src/lk/ijse/AppInitializer.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ant = new AnnotationConfigApplicationContext();
-        ant.getBean(AppConfig.class);
+        ant.register(AppConfig.class);
         ant.refresh();
         ant.registerShutdownHook();
     }
---
+++ SpringFramework/03-Bean Scope/src/lk/ijse/pojo/Pojo1.java
@@ -0,0 +1,10 @@
+package lk.ijse.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Pojo1 {
+    public Pojo1(){
+        System.out.println("Pojo1 Constructor..!");
+    }
+}
