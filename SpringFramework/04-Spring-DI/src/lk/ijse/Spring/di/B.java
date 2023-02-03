3ca29d47b8ea434708f579fd9ef369de38ae98e0
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-03 22:37:20 +0530

    04-Spring-DI : Add Constructor Inject

--- SpringFramework/04-Spring-DI/src/lk/ijse/Spring/di/B.java
+++ SpringFramework/04-Spring-DI/src/lk/ijse/Spring/di/B.java
@@ -6,10 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class B{
 
-    @Autowired
+/*    @Autowired
+    ABC abc;//Property Inject*/
+
     ABC abc;
 
-    public B(){
+    @Autowired
+    public B(ABC a){
+        this.abc=a;//Constructor Inject
         System.out.println("B Constructor..!");
     }
 
