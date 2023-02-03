8715e5ee928ea568b5efcafa94b0637b39b6abc3
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-03 21:42:40 +0530

    04-Spring-DI : Autowired (Loos cople)

--- SpringFramework/04-Spring-DI/src/lk/ijse/Spring/pojo/Boy.java
+++ SpringFramework/04-Spring-DI/src/lk/ijse/Spring/pojo/Boy.java
@@ -1,16 +1,20 @@
 package lk.ijse.Spring.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Boy {
+
+    @Autowired
+    Girl girl;
 
     public Boy(){
         System.out.println("Boy Constructor..!");
     }
 
     public void chatWithGirl(){
-        Girl girl = new Girl();
+       // Girl girl = new Girl();
         girl.Chat();
     }
 }
