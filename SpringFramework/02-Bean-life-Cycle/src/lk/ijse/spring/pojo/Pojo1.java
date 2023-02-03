1923bd5ade273f2a2249a4b6ede0a13a0397a545
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-03 11:36:52 +0530

    setApplicationContext content

--- SpringFramework/02-Bean-life-Cycle/src/lk/ijse/spring/pojo/Pojo1.java
+++ SpringFramework/02-Bean-life-Cycle/src/lk/ijse/spring/pojo/Pojo1.java
@@ -4,10 +4,12 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Pojo1 implements BeanNameAware, BeanFactoryAware {
+public class Pojo1 implements BeanNameAware, BeanFactoryAware, ApplicationContextAware {
     public Pojo1(){
         System.out.println("Pojo1 Constructor");
     }
@@ -20,5 +22,10 @@ public class Pojo1 implements BeanNameAware, BeanFactoryAware {
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println("Bean Factor Aware : "+beanFactory);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("applicationContext Aware : "+applicationContext);
     }
 }
