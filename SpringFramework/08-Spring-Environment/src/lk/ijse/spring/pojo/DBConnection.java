af7d77783da65b9748b0493f59b75ba0b137000c
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-04 16:21:05 +0530

    08-Spring-Environment : Environment variables het

--- SpringFramework/08-Spring-Environment/src/lk/ijse/spring/pojo/DBConnection.java
+++ SpringFramework/08-Spring-Environment/src/lk/ijse/spring/pojo/DBConnection.java
@@ -1,11 +1,25 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DBConnection {
+public class DBConnection implements InitializingBean {
+
+    @Value("${os.name}")   //Property Placeholder
+    public String myOsName;
+
+    @Value("${user.name}")
+    public String myUserName;
 
     public DBConnection(){
         System.out.println("DBConnection constructor..!");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("DBConnection Ocn Name : "+myOsName);
+        System.out.println("DBConnection Ocn myUserName : "+myUserName);
     }
 }
