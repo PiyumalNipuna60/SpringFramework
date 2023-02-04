14222ad94da68451a2e4ef1e4c64bbe3c4517a5f
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-04 11:10:29 +0530

    07-Runtime-Value-Inject : Add Value

--- SpringFramework/07-Runtime-Value-Inject/src/lk/ijse/Spring/config/AppConfig.java
+++ SpringFramework/07-Runtime-Value-Inject/src/lk/ijse/Spring/config/AppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.Spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -10,4 +11,9 @@ public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig Constructor..!");
     }
+
+/*    @Bean
+    public String setD(){
+        return "Hello Nipuna";
+    }*/
 }
--- SpringFramework/07-Runtime-Value-Inject/src/lk/ijse/Spring/pojo/Customer.java
+++ SpringFramework/07-Runtime-Value-Inject/src/lk/ijse/Spring/pojo/Customer.java
@@ -1,10 +1,16 @@
 package lk.ijse.Spring.pojo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Customer {
-    public Customer(){
-        System.out.println("Customer Constructor..!");
+
+   /* public Customer(){
+        System.out.println("Customer Constructor()..!");
+    }*/
+
+    public Customer(@Value("ijse") String name){
+        System.out.println("Customer Constructor(int i)..!"+name);
     }
 }
