eb4c644d81bde9180031a21bdcc9d63a95039471
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-04 16:10:27 +0530

    08-Spring-Environment : Environment variables

--- SpringFramework/08-Spring-Environment/src/lk/ijse/spring/AppInitializer.java
+++ SpringFramework/08-Spring-Environment/src/lk/ijse/spring/AppInitializer.java
@@ -3,6 +3,10 @@ package lk.ijse.spring;
 import lk.ijse.spring.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 public class AppInitializer {
 
     public static void main(String[] args) {
@@ -10,5 +14,23 @@ public class AppInitializer {
         ant.register(AppConfig.class);
         ant.refresh();
         ant.registerShutdownHook();
+
+
+        //OS Environment variables
+        Map<String, String> getenv = System.getenv();
+        for (String key : getenv.keySet()) {
+            String value = getenv.get(key);
+            System.out.println(key+" : "+value);
+        }
+
+        System.out.println("==========================");
+
+
+        //Java Environment variables
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            Object values = properties.get(key);
+            System.out.println(key+" : "+values);
+        }
     }
 }
