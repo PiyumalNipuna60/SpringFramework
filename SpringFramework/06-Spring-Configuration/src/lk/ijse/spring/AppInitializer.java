324ddf7c81a609bdbd5ece71654b0dd3bde03b3a
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-04 10:47:54 +0530

    06-Spring-Configuration : Config part 2

--- SpringFramework/06-Spring-Configuration/src/lk/ijse/spring/AppInitializer.java
+++ SpringFramework/06-Spring-Configuration/src/lk/ijse/spring/AppInitializer.java
@@ -1,7 +1,6 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfige;
-import lk.ijse.spring.config.ConfigeOne;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
@@ -12,7 +11,7 @@ public class AppInitializer {
         //if we have more them to the one config class
         //we can register them to the context as follows
         ant.register(AppConfige.class);
-        ant.register(ConfigeOne.class);
+        /*    ant.register(ConfigeOne.class);*/
         ant.refresh();
         ant.registerShutdownHook();
     }
--- SpringFramework/06-Spring-Configuration/src/lk/ijse/spring/config/AppConfige.java
+++ SpringFramework/06-Spring-Configuration/src/lk/ijse/spring/config/AppConfige.java
@@ -2,9 +2,11 @@ package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.spring")
+@ComponentScan(basePackages = "lk.ijse.spring.pojo")
+@Import({ConfigeOne.class}) // class eka import karaganna
 public class AppConfige {
 
     public AppConfige(){
