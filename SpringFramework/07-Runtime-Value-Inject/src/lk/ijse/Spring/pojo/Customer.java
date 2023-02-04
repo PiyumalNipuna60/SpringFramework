ab6c517f9d9852ef55dd3827bdfa50854333bc12
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-04 11:13:12 +0530

    07-Runtime-Value-Inject : Add Value set Primitive data Type

--- SpringFramework/07-Runtime-Value-Inject/src/lk/ijse/Spring/pojo/Customer.java
+++ SpringFramework/07-Runtime-Value-Inject/src/lk/ijse/Spring/pojo/Customer.java
@@ -10,7 +10,16 @@ public class Customer {
         System.out.println("Customer Constructor()..!");
     }*/
 
-    public Customer(@Value("ijse") String name){
+  /*  public Customer(@Value("ijse") String name){    //String Data
         System.out.println("Customer Constructor(int i)..!"+name);
+    }*/
+
+    //can assign with @Value annotation >
+    //Primitive data Type
+    //String Data
+    //Arrays
+
+    public Customer(@Value("10") int age){    //Primitive data Type
+        System.out.println("Customer Constructor(int i)..! "+age);
     }
 }
