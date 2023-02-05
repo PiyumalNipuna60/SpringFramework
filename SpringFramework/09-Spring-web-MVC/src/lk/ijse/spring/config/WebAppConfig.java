82c7f46f50fb9ab30b887c0c146cbed0a81670e2
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-05 11:12:35 +0530

    Add WebAppInitializer clss and setup

---
+++ SpringFramework/09-Spring-web-MVC/src/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebAppConfig {
+}
