dd01758565dd41c832f0443e9e8f0151b467ec41
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-11 23:21:36 +0530

    Add DController : Duel Wild Card Mapping Introduct

--- SpringFramework/12-Mapping-Spec/src/lk/ijse/spring/controller/FController.java
+++ SpringFramework/12-Mapping-Spec/src/lk/ijse/spring/controller/FController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/five")
+@RequestMapping("/six")
 public class FController {
 
     @GetMapping
@@ -13,6 +13,25 @@ public class FController {
         return "GetMapping invoke..!";
     }
 
+    //Duel Wild Card Mapping
+    //0  or more inside a segment or more segments
 
+
+    //http://localhost:8080/12_Mapping_Spec/six/sss/ss/path/A/B  //wrong
+    //http://localhost:8080/12_Mapping_Spec/six/path//ssA/A/B  //ok
+    //http://localhost:8080/12_Mapping_Spec/six/path//ssA/aaa/A/B  //ok
+
+    @GetMapping("/path/**/A/B")
+    public String testone1(){
+        return "GetMapping Method 1 invoke..!";
+    }
+
+    //http://localhost:8080/12_Mapping_Spec/six/sss/ss/path/A/B  //ok
+    //http://localhost:8080/12_Mapping_Spec/six/sss/path/A/B  //ok
+
+    @GetMapping("/**/path/A/B")
+    public String testone2(){
+        return "GetMapping Method 2 invoke..!";
+    }
 
 }
