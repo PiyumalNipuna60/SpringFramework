3b96f3399623e39889bc2ae4a293265fe389c1b3
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-12 00:15:32 +0530

    Add HController :content type with consumes Introduct

--- SpringFramework/12-Mapping-Spec/src/lk/ijse/spring/controller/HController.java
+++ SpringFramework/12-Mapping-Spec/src/lk/ijse/spring/controller/HController.java
@@ -1,8 +1,11 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("/eight")
@@ -28,4 +31,12 @@ public class HController {
     public String testOne2(){
         return "GetMapping testOne2 invoke..!";
     }
+
+        //       =======or====
+
+        @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+        public String testOne3(){
+            return "GetMapping testOne3 invoke..!";
+        }
+
 }
