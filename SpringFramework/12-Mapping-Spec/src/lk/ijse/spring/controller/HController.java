a51632df077bcc486672e91ab569d78d337586d1
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-12 12:21:56 +0530

    Add Content

--- SpringFramework/12-Mapping-Spec/src/lk/ijse/spring/controller/HController.java
+++ SpringFramework/12-Mapping-Spec/src/lk/ijse/spring/controller/HController.java
@@ -5,17 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
-
 @RestController
 @RequestMapping("/eight")
 public class HController {
 
     @GetMapping
-    public String testOne1() {
-        return "GetMapping invoke..!";
+    public String testOne1(){
+        return "Method eight Invoked";
     }
-
 
     //How many parts are there in an HTTP request.?
     // There are two parts
@@ -26,17 +23,14 @@ public class HController {
     // of its body using headers
     //consume = (this method expect content-type header with the request)
 
-    //    @GetMapping(consumes = {"application/json"})
-    @GetMapping(consumes = {"Application/json"})
-    public String testOne2(){
-        return "GetMapping testOne2 invoke..!";
+//    @GetMapping(consumes = {"application/json"})
+    @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public String testOne(){
+        return "Method One Invoked";
     }
 
-        //       =======or====
-
-        @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
-        public String testOne3(){
-            return "GetMapping testOne3 invoke..!";
-        }
-
+    @GetMapping(consumes = {MediaType.TEXT_HTML_VALUE})
+    public String testTwo(){
+        return "Method Two Invoked";
+    }
 }
--- SpringFramework/12-Mapping-Spec/src/lk/ijse/spring/controller/IController.java
+++ SpringFramework/12-Mapping-Spec/src/lk/ijse/spring/controller/IController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/nine")
 public class IController {
 
-
+@GetMapping
+    public String test(){
+    return "hy";
+}
 
 }
