68daf44996cdd8ecb2a71de6fad1b613e9088c24
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-12 12:08:50 +0530

    Add IController Class

---
+++ SpringFramework/12-Mapping-Spec/src/lk/ijse/spring/controller/IController.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/nine")
+public class IController {
+
+
+
+}
