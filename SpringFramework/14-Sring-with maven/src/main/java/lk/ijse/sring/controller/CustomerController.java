1e9885d5f45f9481fcdb7edcc5f2d12155c31d40
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-14 21:47:51 +0530

    Add module 14 : CustomerController class

---
+++ SpringFramework/14-Sring-with maven/src/main/java/lk/ijse/sring/controller/CustomerController.java
@@ -0,0 +1,11 @@
+package lk.ijse.sring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/customer")
+public class CustomerController {
+}
