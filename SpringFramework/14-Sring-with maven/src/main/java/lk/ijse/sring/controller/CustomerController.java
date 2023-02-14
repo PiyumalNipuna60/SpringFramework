0c716c8564bb202c78d93af480ff3da4e799048f
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-15 00:08:22 +0530

    Add module 14 : Refactor customer controller with front

--- SpringFramework/14-Sring-with maven/src/main/java/lk/ijse/sring/controller/CustomerController.java
+++ SpringFramework/14-Sring-with maven/src/main/java/lk/ijse/sring/controller/CustomerController.java
@@ -4,19 +4,36 @@ import lk.ijse.sring.dto.CustomerDTO;
 import lk.ijse.sring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 @CrossOrigin
 @RequestMapping("/customer")
 public class CustomerController {
 
     @GetMapping
-    public void getCustomer() {
-        System.out.println("CustomerController getCustomer invoke..!");
+    public ResponseUtil getAllCustomer() {
+        ArrayList<CustomerDTO> allCustomer = new ArrayList();
+        allCustomer.add(new CustomerDTO("C001","kamala","galle",2500));
+        allCustomer.add(new CustomerDTO("C002","vimala","matara",3000));
+        allCustomer.add(new CustomerDTO("C003","somapala","colombo",4000));
+        allCustomer.add(new CustomerDTO("C004","gunapala","colombo",5000));
+        return new ResponseUtil("OK","Successfully LoadAll.!",allCustomer);
     }
 
     @PostMapping
     public ResponseUtil saveCustomer(CustomerDTO dto) {
         System.out.println(dto.getId() + " " + dto.getName() + " " + dto.getAddress() + " " + dto.getSalary());
         return new ResponseUtil("OK", "Successfully Registered..!", null);
+    }
+
+    @DeleteMapping(params = {"id"})
+    public ResponseUtil deleteCustomer(String id){
+        return new ResponseUtil("OK", "Successfully Deleted : "+id, null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateCustomer(CustomerDTO dto){
+        return new ResponseUtil("OK", "Successfully Update : "+dto.getId(), null);
     }
 }
