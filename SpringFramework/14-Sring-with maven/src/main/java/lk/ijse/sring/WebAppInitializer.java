5e82d7bdd0278efc6a5297a94ef54dde4d48ce00
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-14 21:37:08 +0530

    Add module 14 : WebAppInitializer class

---
+++ SpringFramework/14-Sring-with maven/src/main/java/lk/ijse/sring/WebAppInitializer.java
@@ -0,0 +1,20 @@
+package lk.ijse.sring;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
