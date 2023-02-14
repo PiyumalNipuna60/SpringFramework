1560a29a7472f10e66303db73eb8fe69591a6c5a
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-14 21:50:29 +0530

    Add module 14 : Content Update

--- SpringFramework/14-Sring-with maven/src/main/java/lk/ijse/sring/config/AppConfig.java
+++ SpringFramework/14-Sring-with maven/src/main/java/lk/ijse/sring/config/AppConfig.java
@@ -1,12 +1,12 @@
 package lk.ijse.sring.config;
 
-import lk.ijse.spring.controller.AController;
+import lk.ijse.sring.controller.CustomerController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan(basePackageClasses = {AController.class})
+@ComponentScan(basePackageClasses = {CustomerController.class})
 @EnableWebMvc
 public class AppConfig {
 }
--- SpringFramework/14-Sring-with maven/src/main/java/lk/ijse/sring/controller/CustomerController.java
+++ SpringFramework/14-Sring-with maven/src/main/java/lk/ijse/sring/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package lk.ijse.sring.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/customer")
 public class CustomerController {
+
+    @GetMapping
+    public void getCustomer(){
+        System.out.println("CustomerController getCustomer invoke..!");
+    }
 }
