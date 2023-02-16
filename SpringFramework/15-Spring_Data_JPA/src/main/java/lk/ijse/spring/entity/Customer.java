4911a9fd42bd4513674aade092d7c16481ceb0d4
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-16 18:07:54 +0530

    15-Spring-with-jpa : Customer Entity

---
+++ SpringFramework/15-Spring_Data_JPA/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -0,0 +1,22 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class Customer {
+    @Id
+    private String id;
+    private String name;
+    private String address;
+    private String salary;
+}
