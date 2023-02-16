1c2709cede329698c6a96ad198bd32912ac86586
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-16 23:03:43 +0530

    Add

--- SpringFramework/15-Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ SpringFramework/15-Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,7 +1,32 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JPAConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource source, JpaVendorAdapter jpv){
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setPackagesToScan("lk.ijse.spring.entity");
+        factoryBean.setDataSource(source);
+        factoryBean.setJpaVendorAdapter(jpv);
+        return factoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource db = new DriverManagerDataSource();
+        db.setDriverClassName("com.mysql.jdbc.Driver");
+        db.setUrl("jdbc:mysql://localhost:3306/db?createDatabaseIfNotExist=true");
+        db.setUsername("root");
+        db.setPassword("1234");
+        return db;
+    }
 }
