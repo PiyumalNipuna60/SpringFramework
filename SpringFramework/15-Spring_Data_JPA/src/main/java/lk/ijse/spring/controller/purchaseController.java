6758065e48ed86afb0a458e980293e06c8135df0
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-02 23:21:56 +0530

    Order save content update

--- SpringFramework/15-Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/purchaseController.java
+++ SpringFramework/15-Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/purchaseController.java
@@ -3,7 +3,10 @@ package lk.ijse.spring.controller;
 import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.dto.OrdersDTO;
 import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.entity.Item;
+import lk.ijse.spring.entity.OrderDetails;
 import lk.ijse.spring.entity.Orders;
+import lk.ijse.spring.repo.ItemRepo;
 import lk.ijse.spring.repo.OrdersRepo;
 import lk.ijse.spring.util.RespondUtil;
 import org.modelmapper.ModelMapper;
@@ -19,14 +22,27 @@ public class purchaseController {
     private OrdersRepo repo;
 
     @Autowired
+    private ItemRepo itemRepo;
+
+    @Autowired
     private ModelMapper mapper;
 
     @PostMapping
     public RespondUtil purchaseOrder(@RequestBody OrdersDTO dto) {
-        System.out.println(dto.toString());
-
         Orders map = mapper.map(dto, Orders.class);
+        if (repo.existsById(map.getOid())){
+            throw new RuntimeException("Order "+map.getOid()+ " Already Exist. Please enter another id..!");
+        }
         repo.save(map);
-        return new RespondUtil();
+
+        for (OrderDetails od : map.getOrderDetails()) {
+            Item item = itemRepo.findById(od.getItemCode()).get();
+            System.out.println(item);
+
+            item.setQtyOnHand(item.getQtyOnHand()-od.getQty());
+            itemRepo.save(item);
+        }
+        return new RespondUtil("OK", "Successfully Registered.!", null);
+
     }
 }
