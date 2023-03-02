437449cda4f10abe5a5cf5e323629859c4a70659
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-02 23:00:44 +0530

    Order save

--- SpringFramework/15-Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/purchaseController.java
+++ SpringFramework/15-Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/purchaseController.java
@@ -3,7 +3,11 @@ package lk.ijse.spring.controller;
 import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.dto.OrdersDTO;
 import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.entity.Orders;
+import lk.ijse.spring.repo.OrdersRepo;
 import lk.ijse.spring.util.RespondUtil;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,9 +15,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/purchase")
 public class purchaseController {
 
+    @Autowired
+    private OrdersRepo repo;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @PostMapping
     public RespondUtil purchaseOrder(@RequestBody OrdersDTO dto) {
         System.out.println(dto.toString());
+
+        Orders map = mapper.map(dto, Orders.class);
+        repo.save(map);
         return new RespondUtil();
     }
 }
